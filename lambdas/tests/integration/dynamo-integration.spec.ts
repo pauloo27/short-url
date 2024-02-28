70a3e775dd74de11bfd2a5c48c145fd0e55c12c9
Paulo <git@pauloo27.dev>
2024-02-28 09:17:47 -0300

    feat: test ordering

--- lambdas/tests/integration/dynamo-integration.spec.ts
+++ lambdas/tests/integration/dynamo-integration.spec.ts
@@ -34,12 +34,39 @@ describe('Integration test with DynamoDB', function () {
         expect(item!.access_count).toBe(0);
 
         const promises = [];
-        for (let i = 0; i < 5; i++) {
+        const accesses = Math.floor(Math.random() * 8) + 1;
+
+        for (let i = 0; i < accesses; i++) {
             promises.push(repo.incrementAccessCount(alias));
         }
         await Promise.all(promises);
 
         const updatedItem = await repo.findOneByAlias(alias);
-        expect(updatedItem!.access_count).toBe(5);
+        expect(updatedItem!.access_count).toBe(accesses);
     }, 10000);
+
+    it('should list ordered by access count', async () => {
+        const repo = new URLRepository(process.env.TEST_TABLE_NAME!);
+        const alias = nanoid();
+        const limit = 5;
+
+        await repo.create({
+            original_url: 'https://www.google.com',
+            alias,
+        });
+
+        await repo.incrementAccessCount(alias);
+
+        const result = await repo.listByAccessCount('AccessCountIndex', limit);
+        expect(result).toBeDefined();
+        expect(result.count).toBeGreaterThanOrEqual(1);
+        expect(result.count).toBeLessThanOrEqual(limit);
+        expect(result.items.length).toBeLessThanOrEqual(limit);
+
+        let previousCount = Number.MAX_SAFE_INTEGER;
+        for (const item of result.items) {
+            expect(item.access_count).toBeLessThanOrEqual(previousCount);
+            previousCount = item.access_count;
+        }
+    });
 });
